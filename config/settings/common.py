61458ec82c5da694d95679af9b8ec2341164cc4b
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-11 16:00:06 -0800

    Mod: Increased 'PAGE_SIZE' value to '50'

--- config/settings/common.py
+++ config/settings/common.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser'
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 50
 }
 
 # Honor the 'X-Forwarded-Proto' navbar for request.is_secure()
