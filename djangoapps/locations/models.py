f6db9e1c04d6db6477ca34a56d093dbe17641467
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-31 13:43:13 -0800

    Mod: Undo changes because I had to make a big change on FE too.

--- djangoapps/locations/models.py
+++ djangoapps/locations/models.py
@@ -1,11 +1,9 @@
-import uuid
-
 from django.db import models
 
 
 class Position(models.Model):
     """ Position Model """
-    uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    uid = models.CharField(max_length=200)
     lng = models.CharField(max_length=60, null=True, blank=True)
     lat = models.CharField(max_length=60, null=True, blank=True)
 
@@ -17,7 +15,7 @@ class Location(models.Model):
     """ Location Model """
     position = models.ForeignKey(Position, null=True, blank=True)
 
-    uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    uid = models.CharField(max_length=200)
     country = models.CharField(max_length=2, blank=True)
     address = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=110, blank=True)
