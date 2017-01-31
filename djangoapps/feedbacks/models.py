8a473ad43d0923b8e75ff58df683e4fbd4f71e48
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-31 12:09:22 -0800

    Mod: Changed CharField to UUIDField type of 'uid'

--- djangoapps/feedbacks/models.py
+++ djangoapps/feedbacks/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Feedback(models.Model):
     """ Feedback Model """
-    uid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=False)
     next_country = models.CharField(max_length=2, blank=True)
 
     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
