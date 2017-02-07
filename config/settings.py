029563371f0ddee484c152054b14600bba926832
sergioruizdavila <sergioruizdavila@gmail.com>
2017-02-06 23:28:04 -0800

    Mod: Prepare to deploy to DEV environment

--- config/settings.py
+++ config/settings.py
@@ -159,9 +159,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
