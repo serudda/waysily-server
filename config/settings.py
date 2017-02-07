7c69224599143769b802391969f15172b7ab7ac8
sergioruizdavila <sergioruizdavila@gmail.com>
2017-02-07 00:05:56 -0800

    Mod: Prepare to deploy to DEV environment

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = '$6(x*g_2g9l_*g8peb-@anl5^*8q!1w)k&e&2!i)t6$s8kia93'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+TEMPLATE_DEBUG = False
+
 ALLOWED_HOSTS = []
 
 # A list of origin hostnames that are authorized to make a cross-site HTTP
