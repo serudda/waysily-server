fac060ac02ec17ea0ac30998eeb0fd8ab7b2d559
sergioruizdavila <sergioruizdavila@gmail.com>
2017-02-06 22:44:24 -0800

    Mod: Prepare to deploy to DEV environment

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
@@ -20,9 +21,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 application = Cling(get_wsgi_application())
 
 # from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
 # from dj_static import Cling
 
 # application = Cling(get_wsgi_application())
 # application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
