e70c7f70d519680f23492cb3ca340688e1c1a8f3
sergioruizdavila <sergioruizdavila@gmail.com>
2017-02-10 18:39:41 -0800

    Mod: Refactor comment on settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ sys.path.insert(0, APPS_ROOT)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$6(x*g_2g9l_*g8peb-@anl5^*8q!1w)k&e&2!i)t6$s8kia93'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! It fine in develop
 DEBUG = True
 
 TEMPLATE_DEBUG = False
