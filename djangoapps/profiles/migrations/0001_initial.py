f73b0e781e73b33f97d5e2a151328b02067b2c0f
sergioruizdavila <sergioruizdavila@gmail.com>
2017-02-23 20:33:19 -0800

    Mod: Prepare to deploy to DEV environment

--- djangoapps/profiles/migrations/0001_initial.py
+++ djangoapps/profiles/migrations/0001_initial.py
@@ -13,7 +13,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0009_auto_20170207_0331'),
         ('locations', '0008_auto_20170224_0101'),
     ]
 
