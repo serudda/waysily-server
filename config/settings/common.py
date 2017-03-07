9e5ed73d5cb90706bd2a20c035b809d98e284f4f
sergioruizdavila <sergioruizdavila@gmail.com>
2017-03-06 23:33:33 -0800

    Mod: Reverse Commit

--- config/settings/common.py
+++ config/settings/common.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'compressor',
     'rest_framework',
-    'corsheaders',
     'argonauts',
     'main',
     'authentication',
@@ -46,7 +45,6 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
