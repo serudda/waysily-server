3a04d8349f352fbc3b80b26203479cc44586fe12
sergioruizdavila <sergioruizdavila@gmail.com>
2017-03-07 20:54:27 -0800

    Test: Testing email sending from SparkPost

--- config/settings.py
+++ config/settings.py
@@ -287,11 +287,12 @@ STATIC_URL = '/static/'
 
 # TODO: Send email using my sparkpost - PRD
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sparkpostmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'SMTP_Injection'
-EMAIL_HOST_PASSWORD = 'c1506170ad8110344c4ed7436888a74b4c1211aa'
+
 EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.sparkpostmail.com'
+EMAIL_HOST_USER = 'help@waysily.com'
+EMAIL_HOST_PASSWORD = 'c1506170ad8110344c4ed7436888a74b4c1211aa'
+EMAIL_PORT = 587
 
 # NOTE: To allow user resets password is necessary create a new 'Site' on Admin
 SITE_ID = 1
