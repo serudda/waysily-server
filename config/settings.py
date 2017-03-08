0a6275abedf7daeca29f9ab53420123c1c6a96d1
sergioruizdavila <sergioruizdavila@gmail.com>
2017-03-07 21:06:47 -0800

    Test: Testing email sending from SparkPost

--- config/settings.py
+++ config/settings.py
@@ -279,20 +279,20 @@ STATIC_URL = '/static/'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # TODO: Send test email using my gmail account
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'sergioruizdavila@gmail.com'
-# EMAIL_HOST_PASSWORD = '.$Gratos2016$.'
-# EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'help@waysily.com'
+EMAIL_HOST_PASSWORD = '.$Waysily2017$.'
+EMAIL_PORT = 587
 
 # TODO: Send email using my sparkpost - PRD
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.sparkpostmail.com'
-EMAIL_HOST_USER = 'SMTP_Injection'
-EMAIL_HOST_PASSWORD = 'c1506170ad8110344c4ed7436888a74b4c1211aa'
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.sparkpostmail.com'
+# EMAIL_HOST_USER = 'SMTP_Injection'
+# EMAIL_HOST_PASSWORD = 'c1506170ad8110344c4ed7436888a74b4c1211aa'
+# EMAIL_PORT = 587
 
 # NOTE: To allow user resets password is necessary create a new 'Site' on Admin
 SITE_ID = 1
