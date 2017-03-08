9d2134a26bdb63d5678497edb6e7244b61c1ba57
sergioruizdavila <sergioruizdavila@gmail.com>
2017-03-07 16:40:17 -0800

    Mod: Prepare to deploy to DEV environment

--- config/settings.py
+++ config/settings.py
@@ -234,7 +234,7 @@ DEV = 'https://waysily-client-dev.herokuapp.com'
 # PRD = 'http://www.waysily.com'
 STAGING = 'https://waysily-client-staging.herokuapp.com'
 PRD = 'https://waysily-client-production.herokuapp.com'
-DOMAIN = PRD
+DOMAIN = DEV
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -276,14 +276,14 @@ STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 # TODO: Show in console when you send a email through app
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # TODO: Send test email using my gmail account
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sergioruizdavila@gmail.com'
-EMAIL_HOST_PASSWORD = '.$Gratos2016$.'
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'sergioruizdavila@gmail.com'
+# EMAIL_HOST_PASSWORD = '.$Gratos2016$.'
+# EMAIL_PORT = 587
 
 # TODO: Send email using my sparkpost - PRD
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
