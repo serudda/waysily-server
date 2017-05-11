5b12c52604bbd20417d0b433f38fb6ab7ef238df
sergioruizdavila <sergioruizdavila@gmail.com>
2017-05-10 21:01:18 -0500

    Mod: Added get country by id view

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
 
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50
 }
 
--- djangoapps/countries/urls.py
+++ djangoapps/countries/urls.py
@@ -15,9 +15,17 @@ country_detail = CountryViewSet.as_view({
     'delete': 'destroy'
 })
 
+country_id_detail = CountryViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
 router = DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     url(r'countries$', country_list, name='country_list'),
-    url(r'countries/(?P<alias_country>.*)$', country_detail, name='country_detail'),
+    url(r'countries/(?P<alias_country>[a-zA-Z]+)$', country_detail, name='country_detail'),
+    url(r'countries/(?P<pk>[0-9]+)$', country_id_detail, name='country_id_detail'),
 ]
